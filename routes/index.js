83459c330ad61daa6263b02f9d1a776dbda8bf47
vsuh <cyx@bk.ru>
2016-01-05 17:40:37 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+/* global tt */
+var express = require('express');
+var router = express.Router();
+var my = require("mysql-native").createTCPClient('mysql.moscollector.local', 3306);
+my.auto_prepare = true;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+function dump_rows(cmd)
+{
+   cmd.addListener('row', function(r) { console.dir(r); } );
+}
+
+function getRowsFromTable(){
+    my.auth('db1cprod','v8','G0bl1n76');
+    var tt = my.query('select * from `upload_settings_chg_proto`;');
+    return tt;    
+}
+//dump_rows(getRowsFromTable());
