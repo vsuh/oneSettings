51e05cf4551e966d2270597a8817bbb04a9adf74
vsuh <cyx@bk.ru>
2016-01-12 17:07:04 +0300

    начало вывода текста логов

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,32 @@
 /* global tt */
 var express = require('express');
 var router = express.Router();
-var my = require("mysql-native").createTCPClient('mysql.moscollector.local', 3306);
-my.auto_prepare = true;
+var fs = require("fs");
+//   fs.writeFileSync(filePath, String, 'utf-8');
+//var my = require("mysql-native").createTCPClient('mysql.moscollector.local', 3306);
+//my.auto_prepare = true;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var stage = req.query.ws;
+  var content = function() {
+  var text = [ ["om", "km", "pl"], ["be", "pl", "ko"], ["ce", "kl", "ok"] ];
+      if (stage == '1') {
+        var fileLog = '//kopt-app-01/com/cmd/dt23/23_b2out_mc_bnu.log';
+        var cnt = fs.readFileSync(fileLog, 'utf8');
+          text.push(['12.01.2016 04:12' , 'Какая - то длинная строка из файла протокола за сегодняшнюю ночь']);
+          text.push(['12.01.2016 04:13' , 'еще Какая- то длинная строка из файла протокола за сегодняшнюю ночь']);
+    //  } elseif(stage == '2') {
+    //      var today = new Date();
+    //      text.push([today, '[&nbsp;]', 'some log text']);
+    //  } else {
+    //      text.push(['today', '[&nbsp;]', 'some log text']);
+      }
+    return text;
+  }
+  res.render('index', { stage: stage , content: content()});
 });
+
 
 
 router.get('/settings', function(req, res, next) {
@@ -35,6 +54,6 @@ function dump_rows(cmd)
 function getRowsFromTable(){
     my.auth('db1cprod','v8','G0bl1n76');
     var tt = my.query('select * from `upload_settings_chg_proto`;');
-    return tt;    
+    return tt;
 }
 //dump_rows(getRowsFromTable());
