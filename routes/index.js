a3b4de6cb35a720c0ef1149f4e296e599ce54016
vsuh <cyx@bk.ru>
2016-01-12 23:48:44 +0300

    begin to use markdown

--- routes/index.js
+++ routes/index.js
@@ -2,57 +2,80 @@
 var express = require('express');
 var router = express.Router();
 var fs = require("fs");
+var Iconv = require('iconv-lite');
 //   fs.writeFileSync(filePath, String, 'utf-8');
 //var my = require("mysql-native").createTCPClient('mysql.moscollector.local', 3306);
 //my.auto_prepare = true;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var stage = req.query.ws;
-  var content = function() {
-  var text = [ ["om", "km", "pl"], ["be", "pl", "ko"], ["ce", "kl", "ok"] ];
-      if (stage == '1') {
-        var fileLog = '//kopt-app-01/com/cmd/dt23/23_b2out_mc_bnu.log';
-        var cnt = fs.readFileSync(fileLog, 'utf8');
-          text.push(['12.01.2016 04:12' , 'Какая - то длинная строка из файла протокола за сегодняшнюю ночь']);
-          text.push(['12.01.2016 04:13' , 'еще Какая- то длинная строка из файла протокола за сегодняшнюю ночь']);
-    //  } elseif(stage == '2') {
-    //      var today = new Date();
-    //      text.push([today, '[&nbsp;]', 'some log text']);
-    //  } else {
-    //      text.push(['today', '[&nbsp;]', 'some log text']);
-      }
-    return text;
-  }
-  res.render('index', { stage: stage , content: content()});
+router.get('/', function (req, res, next) {
+    var stage = req.query.ws || '1';
+    var content = function () {
+    var text = new Array();
+        if (stage == '1') {
+            var fileLog = '//kopt-app-01/com/cmd/dt23/23_b2out_mc_bnu.log';
+            var ctime = fs.statSync(fileLog).ctime;
+            var translated = Iconv.decode(fs.readFileSync(fileLog), 'windows1251');
+            if(!translated) {
+                translated = "Не удалось прочитать логфайл `"+fileLog+"`";
+            }
+            text.push([_tStamp(ctime),'1cKucy', translated]);
+        } else if (stage == '2') {
+            var today = new Date();
+            text.push([_tStamp(today), 'initialized ok']);
+        } else if (stage == '3') {
+            var logFiles = [
+                '//kopt-app-01/com/cmd/log/02_out_1cCORP.dt.log',
+                '//kopt-app-01/com/cmd/log/02_out_1cKucy.dt.log',
+                '//kopt-app-01/com/cmd/log/02_out_1cUAT.dt.log'
+            ];
+            logFiles.forEach(function(fn, index, array){
+                var ct = fs.statSync(fn).ctime;
+                var rt = 'out_(.*)\.dt.*$';
+                var re = RegExp(rt);
+                var ib = re.exec(fn);
+                var translated = Iconv.decode(fs.readFileSync(fn), 'windows1251');
+                text.push([_tStamp(ct), ib[1], translated]);
+            //    console.log(_tStamp(ct), ib[1]);
+            });
+
+        } else {
+            var today = new Date();
+            text.push([_tStamp(today), 'not implemented yet']);
+  
+        }
+        return text;
+    }
+    res.render('index', { stage: stage, content: content() });
+});
+
+function _tStamp(tm){
+    return '<div style="white-space:nowrap;">'+tm.toLocaleDateString() + "&nbsp;" + tm.toLocaleTimeString()+'</div>'; 
+}
+
+router.get('/settings', function (req, res, next) {
+    res.render('settings', { title: 'Express' });
 });
 
 
-
-router.get('/settings', function(req, res, next) {
-  res.render('settings', { title: 'Express' });
+router.get('/params', function (req, res, next) {
+    res.render('params', { title: 'Express' });
 });
 
 
-router.get('/params', function(req, res, next) {
-  res.render('params', { title: 'Express' });
-});
-
-
-router.get('/customers', function(req, res, next) {
-  res.render('customers', { title: 'Express' });
+router.get('/customers', function (req, res, next) {
+    res.render('customers', { title: 'Express' });
 });
 
 
 module.exports = router;
 //// *** *** *** ////
-function dump_rows(cmd)
-{
-   cmd.addListener('row', function(r) { console.dir(r); } );
+function dump_rows(cmd) {
+    cmd.addListener('row', function (r) { console.dir(r); });
 }
 
-function getRowsFromTable(){
-    my.auth('db1cprod','v8','G0bl1n76');
+function getRowsFromTable() {
+    my.auth('db1cprod', 'v8', 'G0bl1n76');
     var tt = my.query('select * from `upload_settings_chg_proto`;');
     return tt;
 }
